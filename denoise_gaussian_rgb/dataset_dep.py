6a44bb1311e33771d7b88c53bcebde8d3f5e90cb
w.wang <67496459+wangweiSJTU@users.noreply.github.com>
2021-12-13 09:07:09 +0800

    Update dataset_dep.py

--- denoise_gaussian_rgb/dataset_dep.py
+++ denoise_gaussian_rgb/dataset_dep.py
@@ -1,15 +1,18 @@
 import torch.utils.data as data
 import torch,pdb
 import h5py
+import random
+
 
 class DatasetFromHdf5(data.Dataset):
-    def __init__(self, file_path):
-        super(DatasetFromHdf5, self).__init__()
-        hf = h5py.File(file_path)
-        self.data = hf.get("label")
+	def __init__(self, file_path):
+		super(DatasetFromHdf5, self).__init__()
+		hf = h5py.File(file_path)
+		self.data = hf.get("label")
 
-    def __getitem__(self, index):
-        return torch.from_numpy(self.data[index,:,:,:]).float()
+	def __getitem__(self, index):
+		r=random.randint(0,self.data.shape[0]-1)
+		return torch.from_numpy(self.data[index,:,:,:]).float(), torch.from_numpy(self.data[r,:,:,:]).float()
 
-    def __len__(self):
-        return self.data.shape[0]
+	def __len__(self):
+		return self.data.shape[0]
